bb2dc5cbee8cfcdafa4695a9af978758ce2c35d3
luisalfred15 <luis.maestres@correo.unimet.edu.ve>
2022-05-02 17:56:34 -0400

    Prueba

---
+++ Ejercicios/ejer0428.py
@@ -0,0 +1,23 @@
+# print('Bienvenido a la calculadora')
+# numero1 = input('Escribe un numero \n') # 
+# numero2 = input('Escribe otro numero \n')
+
+# if (numero1.isnumeric() and numero2.isnumeric()):
+#     numero1 = float(numero1)
+#     numero2 = float(numero2)
+#     operacion = input('Selecciona una operacion \n')
+# else:
+#     print('Escriba valores validos')
+
+# if operacion == 'suma':
+#     print('El resultado es ' + numero1 + numero2)
+# if operacion == 'resta':
+#     print('El resultado es ' + numero1 - numero2)
+# if operacion == 'multiplicacion':
+#     print('El resultado es ' + numero1 * numero2)
+# if operacion == 'division': 
+#     if numero2 != 0:
+#     print('El resultado es ' + numero1 / numero2)
+#     else:
+#         print('Error: El numero 2 no puede ser cero')
+
---
+++ Ejercicios/primos.py
@@ -0,0 +1,6 @@
+print('Bienvenido a la calculadora de primos')
+
+numero = int(input('Por favor, ingresa un numero para verificar'))
+aux = numero
+
+while  > :
