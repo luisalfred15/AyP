3cb7328918c57abe7ba300fd6f105f4b1b6984f2
luisalfred15 <luis.maestres@correo.unimet.edu.ve>
2022-05-04 19:02:08 -0400

    Ejer triangulo resuelto

--- Clases/triangulo.py
+++ Clases/triangulo.py
@@ -1,6 +1,16 @@
-lineas = int(input('Introduce un numero para generar el triangulo: '))
-i = lineas
+filas = int(input('Introduce el numero filas para el triangulo: '))
+fila_actual = 1
+numero_actual = 1
 
-
-
-while 
+if filas > 0:
+    print(numero_actual)
+    while fila_actual < filas:
+        numero_actual += 2
+        i = numero_actual
+        while i > 0:
+            if i == 1:
+                print(i)
+            else:
+                print(i,end=' ')
+            i -= 2
+        fila_actual += 1
