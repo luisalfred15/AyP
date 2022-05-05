3f0d3bd2f4f28d64dbdc452cce90320b51724081
luisalfred15 <luis.maestres@correo.unimet.edu.ve>
2022-05-04 21:04:19 -0400

    Ejercicio resuelto

---
+++ Ejercicios/palindromo.py
@@ -0,0 +1,21 @@
+entrada = input('Ingrese un numero positivo o palabra: ')
+i = 0
+datos_iguales = True
+
+while i <= len(entrada) // 2:
+    dato_1 = entrada[-len(entrada)+i]
+    dato_2 = entrada[len(entrada)-i-1]
+    if dato_1 != dato_2:
+        datos_iguales = False
+    i += 1
+
+if datos_iguales:
+    if entrada.isnumeric():
+        print('El numero ingresado es un palindromo')
+    else:
+        print('La palabra ingresada es un palindromo')
+else:
+    if entrada.isnumeric():
+        print('El numero ingresado NO es un palindromo')
+    else:
+        print('La palabra ingresada NO es un palindromo')
