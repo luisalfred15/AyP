99ebb48a3e36ea1de8b37cff91a3b3b3490fd383
luisalfred15 <luis.maestres@correo.unimet.edu.ve>
2022-05-10 21:33:35 -0400

    Abundante realizado

--- Clases/abundante.py
+++ Clases/abundante.py
@@ -1,1 +1,11 @@
 numero = int(input('Ingrese un numero: '))
+suma_divisores = 0
+
+for i in range(1,numero):
+    if numero % i == 0:
+        suma_divisores = suma_divisores + i
+
+if suma_divisores > numero:
+    print('El numero es abundante')
+else:
+    print('El numero NO es abundante')
