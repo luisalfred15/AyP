a0938b89a6c3e915e8a5c2cbc72415bb00c4038c
luisalfred15 <luis.maestres@correo.unimet.edu.ve>
2022-05-11 20:48:42 -0400

    Calculadora de primos creada

--- Ejercicios/primos.py
+++ Ejercicios/primos.py
@@ -1,10 +1,14 @@
-numero = int(input('Ingrese el numero para averiguar si es primo: '))
-i = numero
+number = int(input('Please enter a number to verify if it is prime: '))
+aux = 0                             # 'aux' will count how many dividers have 'number'
 
-while i < numero:
-    division = numero / i
-    if division.is_integer() and (numero == i or i == 1):
-        print('El ', numero, 'es primo')
-    else:
-        print('El ', numero, 'NO es primo')
-    i += 1
+if number < 0:
+    print('Please enter a valid number')
+else:
+    for i in range(1,number+1):
+        if number % i == 0:
+            aux += 1                # When found a divisor of 'number', 'aux' will count it
+
+if aux == 2:                        # Since primes only have two divisors, 1 and themselves, when 'aux' is 2, then 'number' is prime
+    print(number,'is prime')
+else:
+    print(number,'is not prime')
